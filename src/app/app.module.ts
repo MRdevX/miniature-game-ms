f1fb6083a5a335d14e93438f0fa1af13b2721597
Mahdi Rashidi <m8rashidi@gmail.com>
2021-09-28 00:05:38 +0300

    feat: add publisher module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from '@root/app/core/core.module';
 import { UserModule } from '@root/app/user/user.module';
 import { GameModule } from './game/game.module';
+import { PublisherModule } from './publisher/publisher.module';
 
-const modules = [UserModule, GameModule];
+const modules = [UserModule, GameModule, PublisherModule];
 
 @Module({
   imports: [CoreModule, ...modules],
