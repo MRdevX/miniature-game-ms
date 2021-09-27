852def229ef490c13d040e613bc7c150c59bb38f
Mahdi Rashidi <m8rashidi@gmail.com>
2021-09-28 00:03:20 +0300

    feat: add game module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '@root/app/core/core.module';
 import { UserModule } from '@root/app/user/user.module';
+import { GameModule } from './game/game.module';
 
-const modules = [UserModule];
+const modules = [UserModule, GameModule];
 
 @Module({
   imports: [CoreModule, ...modules],
