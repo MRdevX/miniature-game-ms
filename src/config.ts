777ead532be7905d50a8efa58dba0d9db33e230a
Mahdi Rashidi <m8rashidi@gmail.com>
2021-10-03 20:27:09 +0300

    feat: add docker support

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const config = convict({
     host: {
       doc: 'The application host url',
       format: 'url',
-      default: '127.0.0.1',
+      default: 'localhost',
       env: 'HOST',
     },
     port: {
